da565f2f0dbbbdf0debaa3a35ae804bd0c263d25
sato0208 <kujira028@gmail.com>
2020-08-31 11:59:26 +0900

    制御構造の別の書き方

--- work/index.php
+++ work/index.php
@@ -16,13 +16,13 @@
     </head>
     <body>
       <ul>
-        <?php if (empty($names)) { ?>
+        <?php if (empty($names)): ?>
           <li>Nobody!</li>
-        <?php } else { ?>
-        <?php foreach ($names as $name){ ?>
+        <?php else: ?>
+        <?php foreach ($names as $name): ?>
           <li><?= h($name); ?></li>
-        <?php } ?>
-        <?php } ?>
+        <?php endforeach ?>
+        <?php endif ?>
       </ul>
     </body>
   </thml>
