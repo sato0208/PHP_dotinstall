a825ee2384b31ab7ea78ca8c4da1a0ac718e6862
sato0208 <kujira028@gmail.com>
2020-08-31 13:46:15 +0900

    ラジオボタンから値を受け取る

--- web/index.php
+++ web/index.php
@@ -6,12 +6,13 @@
   // 読み込みに失敗しても処理は止まらない
   include('../app/_parts/_header.php');
 
+
 ?>
 
 <form action="result.php" method="get">
-  <label><input type="checkbox" name="colors[]" value="orange"> Orange</nput></label>
-  <label><input type="checkbox" name="colors[]" value="pink"> Pink</nput></label>
-  <label><input type="checkbox" name="colors[]" value="gold"> Gold</nput></label>
+  <label><input type="radio"" name="color" value="orange"> Orange</nput></label>
+  <label><input type="radio" name="color" value="pink"> Pink</nput></label>
+  <label><input type="radio" name="color" value="gold"> Gold</nput></label>
   <button>Send</button>
 </form>
 
--- web/result.php
+++ web/result.php
@@ -3,9 +3,10 @@
   // 読み込みに失敗したら止めて欲しい
   require('../app/functions.php');
 
-  $colors = filter_input(INPUT_GET, 'colors', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
-  ;
-  $colors = empty($colors) ? 'None selected' : implode(',', $colors);
+  // $color = filter_input(INPUT_GET, 'color');
+
+  // $colorが空白の場合はNone selectedと表示させる
+  $color = filter_input(INPUT_GET, 'color') ?? 'None selected';
 
   // 読み込みに失敗しても処理は止まらない
   include('../app/_parts/_header.php');
@@ -13,7 +14,7 @@
 ?>
 
 
-<p><?= h($colors); ?></p>
+<p><?= h($color); ?></p>
 <p><a href="index.php">Go back</a></p>
 
 <?php
