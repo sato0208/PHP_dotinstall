fc8c9a639dbcc57731e8613b918ebef944887117
sato0208 <kujira028@gmail.com>
2020-08-31 12:28:10 +0900

    フォームで値を送信する

---
+++ web/index.php
@@ -0,0 +1,17 @@
+<?php
+
+  // 読み込みに失敗したら止めて欲しい
+  require('../app/functions.php');
+
+  // 読み込みに失敗しても処理は止まらない
+  include('../app/_parts/_header.php');
+
+?>
+
+<form action="result.php" method="get">
+  <input type="text" name="message">
+  <button>Send</button>
+</form>
+
+<?php
+  include('../app/_parts/_footer.php');
--- web/result.php
+++ web/result.php
@@ -8,7 +8,8 @@
 
 ?>
 
-   
+<p><?= h($message); ?></p>
+<p><a href="index.php">Go back</a></p>
 
 <?php
   include('../app/_parts/_footer.php');
