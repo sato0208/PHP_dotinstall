383847071b43dd18c5fd444f6493c527b6d6029c
sato0208 <satochiharu@satonoMacBook-Pro.local>
2020-08-31 11:15:16 +0900

    日付を表示させる

---
+++ work/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+  <thml lang="ja">
+    <head>
+      <meta charset="utf-8">
+      <title>PHP Practice</title>
+    </head>
+    <body>
+      <p>Hello, PHP!</p>
+      <p>Today: <?php echo date('Y-m-d H:i:s l'); ?></p>
+    </body>
+  </thml>
